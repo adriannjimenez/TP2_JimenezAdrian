e872377c30ca64864774ea84b2e4fbdeacc534a0
adriannjimenez <adriannjimenez@mi.unc.edu.ar>
2026-06-07 19:45:15 -0300

    Se agregan esjercicios 2 y 3. Faltan 17 todavía

---
+++ 2-Calcular el área de un rectángulo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _2_Calcular_el_área_de_un_rectángulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float lado_base;
+            float altura;
+            float area;
+            Console.Clear();
+            Console.WriteLine("CÁLCULO DEL ÁREA DE UN RECTÁNGULO");
+            Console.WriteLine("CUIDADO: El punto decimal es la coma \",\"");
+            Console.WriteLine();
+            Console.Write("Ingrese la medida de la BASE [cm]: ");
+            lado_base = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Ingrese la medida de la ALTURA [cm]: ");
+            altura = float.Parse(Console.ReadLine());
+            area = lado_base * altura;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("EL valor de la base es " + lado_base + " cm.");
+            Console.WriteLine("EL valor de la altura es " + altura + " cm.");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("El área es igual a {0} cm^2:",area);
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para finalizar!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3- Conversión de temperatura/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _3__Conversión_de_temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ciudad;
+            float tCelsius;
+            float tFahrenheit;
+            Console.Clear();
+            Console.Write("Ingrese el nombre de la ciudad: ");
+            ciudad = Console.ReadLine();
+            Console.Write("Ingrese la temperatura en Celsius: ");
+            tCelsius = float.Parse(Console.ReadLine());
+            tFahrenheit = (tCelsius * (9f / 5f)) + 32f;
+            Console.WriteLine("----------------------");
+            Console.WriteLine("En {0}, la temperatura es de {1}°C", ciudad, tCelsius);
+            Console.WriteLine("que se corresponde a " + tFahrenheit + "ºF.");
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para finalizar!");
+            Console.ReadKey();
+        }
+    }
+}
