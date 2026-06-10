f63ff58f420edcb712d122e5506f73353fc2b24c
adriannjimenez <adriannjimenez@mi.unc.edu.ar>
2026-06-10 14:03:21 -0300

    Se vuelve a subir acts 1 a 3

---
+++ 1_Registrar_datos_personales/1_Registrar_datos_personales/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1_Registrar_datos_personales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            byte edad;
+            Console.Clear();
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido:");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+            edad = byte.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Felicitaciones {0} {1}. Cumplirás {2} años", nombre, apellido, edad + 1);
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para finalizar!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3__Conversión_de_temperatura/3__Conversión_de_temperatura/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _3__Conversión_de_temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ciudad;
+            float tCelsius;
+            Console.Clear();
+            Console.Write("Ingrese el nombre de la ciudad: ");
+            ciudad = Console.ReadLine();
+            Console.Write("Ingrese la temperatura en Celsius: ");
+            tCelsius = float.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------");
+            Console.WriteLine("En {0}, la temperatura es de {1}°C", ciudad, tCelsius);
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para finalizar!");
+            Console.ReadKey();
+        }
+    }
+}
